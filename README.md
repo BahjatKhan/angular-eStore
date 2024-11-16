2e6f006ae311ff88394905ffe131ddad50f4e757
Bahjat Khan <bahjat_khan2002@yahoo.com>
2024-11-16 04:45:49 -0500

    generated the project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EStore
+# HotelinventoryApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'eStore' title`, () => {
+  it(`should have the 'hotelinventory-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eStore');
+    expect(app.title).toEqual('hotelinventory-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, eStore');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hotelinventory-app');
   });
 });
